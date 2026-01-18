93e5f24a8bf0a7209c0541ca37dd4534cf11a4d3
soummodeb2398-beep <soummodeb2398@gmail.com>
2026-01-18 20:19:17 +0600

    sixth

--- Build a counter app/index.js
+++ Build a counter app/index.js
@@ -2,9 +2,20 @@
 let count = 0
 let countEl = document.getElementById("count-el")
 
+let p = document.getElementById("p")
 function increment() {
    
     count = count + 1
     countEl.innerText = count;
     console.log(count)
+}
+
+
+function save() {
+    let c = count + "-";
+    p.innerText += c
+    //p.textContent +=c
+    countEl.innerText = 0
+    count = 0
+  console.log(c)
 }
