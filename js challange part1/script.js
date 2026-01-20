f8860dc7a16e50933ce13dc83935d480c851ba85
soummodeb2398-beep <soummodeb2398@gmail.com>
2026-01-20 19:33:10 +0600

    seventh

---
+++ js challange part1/script.js
@@ -0,0 +1,7 @@
+let error = document.getElementById("error"); // Fixed the function name
+console.log(error);
+
+function purches() {
+    console.log("button clicked");
+    error.textContent = "Something went wrong";
+}
---
+++ js challange part1/target.js
@@ -0,0 +1,16 @@
+let fname = "soummo";
+let lastname = "deb";
+let thirdname = fname + " " + lastname;
+function greet() {
+  console.log(thirdname);
+}
+greet();
+greet();
+greet();
+greet();
+console.log("2" + 2)//22
+console.log(11 + 7)//18
+console.log(6 + "5")//65
+console.log("My key: " + 5 + 8);//My key: 58
+console.log(2 + 2)//4
+console.log("11" + "14")//1114
