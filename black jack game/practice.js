0eea44456159580302da95f225c254ea1fe0aae0
soummodeb2398-beep <soummodeb2398@gmail.com>
2026-01-26 18:47:29 +0600

    seventh

---
+++ black jack game/practice.js
@@ -0,0 +1,28 @@
+let list = ["check out my netflix clone", "hii i am there ", "share the portfolio"]
+let marks = [10, 76, 78]
+let experience = ["school boy", "engineer"]
+console.log(marks.length)
+let per = ["soummo", 35, true]
+let cards = [6, 7]
+cards.push(9)
+console.log(cards)
+cards.pop()
+console.log(cards);
+for (let i = 10; i <= 100; i += 10){
+    console.log(i);
+    
+}
+for (let i = 0; i < list.length; i++){
+    console.log(list[i])
+}
+let random = Math.random()// genarates number between 0 to 1(0 - 0.999)
+let floorednum = Math.floor(5.8773)
+let mix = Math.floor(Math.random()*6)
+console.log(floorednum)
+// make a ludo function
+function ludo() {
+    let random = Math.floor(Math.random() * 6) + 1
+    return random
+}
+let r = ludo()
+console.log(r)
--- black jack game/script.js
+++ black jack game/script.js
@@ -1,6 +1,8 @@
-let first = 10
-let second = 6
-let sum = first + second
+let first = getrandomcard();
+let second = getrandomcard();
+let cards = [first, second]
+let sum =  cards[0] + cards[1]
+
 let blackjack = false
 let isalive = true
 let message = ""
@@ -11,7 +13,15 @@ let sumel = document.getElementById("sum-el")
 console.log(sumel)
 let cardel = document.getElementById("card-el")
 
- 
+function getrandomcard() {
+   
+  let random = Math.floor(Math.random() * 13) + 1;
+  if (random === 1) return 11;
+  else if (random >= 11 && random <= 13) return 10;
+  else return random
+
+
+ }
 function start() {
     if (sum < 20) {
       message = "Do you want to draw a new card? ";
@@ -21,11 +31,21 @@ function start() {
     } else {
       message = "you are out of the game";
       isalive = false;
-    }
+  }
+  
     messageel.textContent = message
     sumel.textContent = "Sum: " + sum
-
+            cardel.textContent = "Cards: " + cards[0] + " " + cards[1]
 }
+
 function card() {
-        cardel.textContent = "Cards: " + first + " , " + second;
+  let card = getrandomcard();
+  sum += card
+  cards.push(card)
+  start()
+
+    cardel.textContent = "Cards: "
+  for (let i = 0; i < cards.length; i++){
+      cardel.textContent += cards[i] + " "
+  }
 }
