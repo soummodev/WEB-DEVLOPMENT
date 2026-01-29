0064599e9c55552350b37468b7a6210885517ae6
soummodeb2398-beep <soummodeb2398@gmail.com>
2026-01-29 21:32:32 +0600

    eighth

---
+++ js challange 2/index.js
@@ -0,0 +1,27 @@
+
+// let info = {
+//     name: "soummo",
+//     age: 23,
+//     country:"Bangladesh"
+// }
+// let formate = info.name + " is " + info.age + " years old and lives in " + info.country
+// function logdata() {
+    
+// console.log(formate)
+// }
+// logdata()
+// let con = ["bangladesh", "india", "pakistan"]
+// con.push("aus")
+// console.log(con)
+// con.pop();
+// console.log(con);
+// con.shift();
+// console.log(con);
+// con.unshift("zim");
+// console.log(con);
+let hands = ["rock", "paper", "scissor"]
+function game() {
+    let point = Math.floor(Math.random() * 3)
+    return hands[point]
+}
+console.log(game())
---
+++ js challange 2/tempCodeRunnerFile.js
@@ -0,0 +1,21 @@
+
+// let info = {
+//     name: "soummo",
+//     age: 23,
+//     country:"Bangladesh"
+// }
+// let formate = info.name + " is " + info.age + " years old and lives in " + info.country
+// function logdata() {
+    
+// console.log(formate)
+// }
+// logdata()
+// let con = ["bangladesh", "india", "pakistan"]
+// con.push("aus")
+// console.log(con)
+// con.pop();
+// console.log(con);
+// con.shift();
+// console.log(con);
+// con.unshift("zim");
+// console.log(con);
