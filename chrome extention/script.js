6b6beb3dadd2d2f419e6203184f504f85dd5fa1b
soummodeb2398-beep <soummodeb2398@gmail.com>
2026-01-31 11:00:30 +0600

    ninth

---
+++ chrome extention/script.js
@@ -0,0 +1,16 @@
+let myleads = []
+const inputel = document.getElementById("input-el")
+const ulel = document.getElementById("ul-el")
+
+const inputbtn = document.getElementById("ibtn");
+inputbtn.addEventListener("click", function () {
+    myleads.push(inputel.value)
+    renderlist()
+});
+function renderlist() {
+    let listitem = "";
+    for (let i = 0; i < myleads.length; i++) {
+      listitem += "<li>" + myleads[i] + "</li>";
+    }
+    ulel.innerHTML = listitem;
+}
