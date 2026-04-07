28177978d880b3d6b9f5dd42a9c3095bb3ac1439
soummodev <soummodeb2399@gmail.com>
2026-04-07 18:53:47 +0600

    seventeen

--- methods and loops/index.js
+++ methods and loops/index.js
@@ -3,3 +3,14 @@ const get = (amount) => {
     return `warning! you just spent £${amount}`
 }
 console.log(get(150))
+function setpermission(permissionlevel, ...names) {
+    console.log(`${names} are approved ${permissionlevel}  level permission `)
+}
+setpermission("admin", "soummo", "deepon")
+function notification(notificationfn) {
+    notificationfn()
+}
+const emnotification = () => console.log("email sent")
+const smsnotification = () => console.log("sms sent")
+notification(emnotification)
+notification(smsnotification);
